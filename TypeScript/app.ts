3c162be389fdd8578f4c07c1f771975b3f1a41fa
Gabriel Silva <gabriel.silva@squadra.com.br>
2019-11-18 10:55:07 -0300

    Ajuste

--- TypeScript/app.ts
+++ TypeScript/app.ts
@@ -2,7 +2,6 @@ let message: String = "Help me, Obi-Wan Kenobi. You'me my only hope!"
 console.log(message);
 
 let episode: number = 4;
-episode = "4";
 console.log("This is episode " + 4);
 episode = episode +1;
 console.log("Next episode is " + episode);
--- TypeScript/dist/app.js
+++ TypeScript/dist/app.js
@@ -1,7 +1,6 @@
 var message = "Help me, Obi-Wan Kenobi. You'me my only hope!";
 console.log(message);
 var episode = 4;
-episode = "4";
 console.log("This is episode " + 4);
 episode = episode + 1;
 console.log("Next episode is " + episode);
