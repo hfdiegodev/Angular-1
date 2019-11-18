4cce3b727749552f5e0f5822feae2cc70753d86e
Gabriel Silva <gabriel.silva@squadra.com.br>
2019-11-18 10:50:06 -0300

    Primeiro exemplo TypeScript

---
+++ TypeScript/app.ts
@@ -0,0 +1,12 @@
+let message: String = "Help me, Obi-Wan Kenobi. You'me my only hope!"
+console.log(message);
+
+let episode: number = 4;
+episode = "4";
+console.log("This is episode " + 4);
+episode = episode +1;
+console.log("Next episode is " + episode);
+
+let favoriteDroid: String;
+favoriteDroid = "T-800";
+console.log("My favorite droid is " + favoriteDroid);
