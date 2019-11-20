0668dc1930c0d71ce351366ed9afd181fc411c07
Gabriel Silva <gabriel.silva@squadra.com.br>
2019-11-20 11:16:11 -0300

    Utilizando classes e interfaces

--- TypeScript/Classes e interfaces/app.ts
+++ TypeScript/Classes e interfaces/app.ts
@@ -1,9 +1,46 @@
 class NaveEspacial{
-    constructor(public propulsor: string){}
+    constructor(public propulsor: string, public cargoContainer: number){
+    }
 
     jumpIntoHyperSpace(){
         console.log(`Entrando no hiperespaço com ${this.propulsor}`)
     }
 }
-let ship = new NaveEspacial("HyperDrive")
-ship.jumpIntoHyperSpace
+
+let ship = new NaveEspacial("HyperDrive", 5)
+// ship.jumpIntoHyperSpace()
+
+class MilleniumFalcon extends NaveEspacial implements ContainerShip{
+
+    constructor(public propulsor: string, public cargoContainer: number){
+        super("", null)
+    }
+    jumpIntoHyperSpace(){
+        if (Math.random() > 0.5) {
+            super.jumpIntoHyperSpace()
+        }else{
+            console.log("Falha ao entrar no hiperespaço")
+        }
+    }
+}
+
+class Foquete extends MilleniumFalcon implements ContainerShip{
+    constructor(){
+        super("HyperDrive", 9)
+    }
+}
+let falcon = new MilleniumFalcon("HyperDrive", 1)
+falcon.jumpIntoHyperSpace()
+
+let fog = new Foquete()
+fog.jumpIntoHyperSpace();
+
+
+interface ContainerShip{
+    cargoContainer?: number
+}
+
+let naveCarga = (ship: ContainerShip) => ship.cargoContainer > 2
+console.log(`Falcon é uma boa nave para carga? ${naveCarga(falcon) ? 'Yes' : 'No' }`)
+console.log(`Foguete é uma boa nave para carga? ${naveCarga(fog) ? 'Yes' : 'No'}`)
+
--- TypeScript/Classes e interfaces/dist/app.js
+++ TypeScript/Classes e interfaces/dist/app.js
@@ -1,11 +1,57 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var NaveEspacial = /** @class */ (function () {
-    function NaveEspacial(propulsor) {
+    function NaveEspacial(propulsor, cargoContainer) {
         this.propulsor = propulsor;
+        this.cargoContainer = cargoContainer;
     }
     NaveEspacial.prototype.jumpIntoHyperSpace = function () {
         console.log("Entrando no hiperespa\u00E7o com " + this.propulsor);
     };
     return NaveEspacial;
 }());
-var ship = new NaveEspacial("HyperDrive");
-ship.jumpIntoHyperSpace;
+var ship = new NaveEspacial("HyperDrive", 5);
+// ship.jumpIntoHyperSpace()
+var MilleniumFalcon = /** @class */ (function (_super) {
+    __extends(MilleniumFalcon, _super);
+    function MilleniumFalcon(propulsor, cargoContainer) {
+        var _this = _super.call(this, "", null) || this;
+        _this.propulsor = propulsor;
+        _this.cargoContainer = cargoContainer;
+        return _this;
+    }
+    MilleniumFalcon.prototype.jumpIntoHyperSpace = function () {
+        if (Math.random() > 0.5) {
+            _super.prototype.jumpIntoHyperSpace.call(this);
+        }
+        else {
+            console.log("Falha ao entrar no hiperespaço");
+        }
+    };
+    return MilleniumFalcon;
+}(NaveEspacial));
+var Foquete = /** @class */ (function (_super) {
+    __extends(Foquete, _super);
+    function Foquete() {
+        return _super.call(this, "HyperDrive", 9) || this;
+    }
+    return Foquete;
+}(MilleniumFalcon));
+var falcon = new MilleniumFalcon("HyperDrive", 1);
+falcon.jumpIntoHyperSpace();
+var fog = new Foquete();
+fog.jumpIntoHyperSpace();
+var naveCarga = function (ship) { return ship.cargoContainer > 2; };
+console.log("Falcon \u00E9 uma boa nave para carga? " + (naveCarga(falcon) ? 'Yes' : 'No'));
+console.log("Foguete \u00E9 uma boa nave para carga? " + (naveCarga(fog) ? 'Yes' : 'No'));
